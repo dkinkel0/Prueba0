db49c49454051c83c53c216ff174b25c8a7c2f50
dkinkel0 <darioekinkela@gmail.com>
2022-07-02 13:10:58 -0300

    Prueba de pusheo

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -3,6 +3,7 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
+  //prueba de pusheo
   sequelize.define('country', {
     name: {
       type: DataTypes.STRING,
